a5472ee85ab8de0cc176e71f3548714072bda70f
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-22 17:08:22 -0500

    Die complete w/ Tests, DiceGames/Tests in progress

--- src/main/java/io/zipcoder/casino/Die.java
+++ src/main/java/io/zipcoder/casino/Die.java
@@ -1,5 +1,32 @@
 package io.zipcoder.casino;
 
 public class Die {
-    private Integer Value;
+
+    public Integer diceValue;
+    public Integer diceNum;
+
+
+    public Die() {
+        diceValue = 6;
+        diceNum = 1;
+    }
+
+
+    public Die(int numofDice) {
+
+        diceValue = numofDice * 6;
+        diceNum = numofDice;
+
+
+    }
+
+
+
+    public Integer getDiceValue() {
+        return diceValue;
+    }
+
+    public Integer getDiceNum() {
+        return diceNum;
+    }
 }
