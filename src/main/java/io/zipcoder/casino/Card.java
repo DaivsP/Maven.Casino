19b44b1a9a900139bedddb3c45d93b3b90e0a66c
Davis Perchik <davisperchik@gmail.com>
2019-02-22 17:11:36 -0500

    working on Card. pulling down Dev

--- src/main/java/io/zipcoder/casino/Card.java
+++ src/main/java/io/zipcoder/casino/Card.java
@@ -1,7 +1,23 @@
 package io.zipcoder.casino;
 
 public class Card {
-    private Enum suit;
-    private Enum rank;
+    private enum Suit {HEARTS, DIAMONDS, CLUBS, SPADES}
+
+    private enum Rank {TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE}
+
     private Boolean hidden;
+    private Suit cardSuit;
+    private Rank cardRank;
+
+    public Card (Suit cardSuit, Rank cardRank){
+        this.cardRank = cardRank;
+        this.cardSuit = cardSuit;
+    }
+
+    public void createCard(){
+        Card newCard = new Card(Suit.HEARTS, Rank.KING);
+    }
+
 }
+
+
