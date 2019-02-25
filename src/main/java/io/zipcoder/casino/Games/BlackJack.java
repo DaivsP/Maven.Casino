40f0a2975a24311ebf59383e7d377158c13fbb77
Davis Perchik <davisperchik@gmail.com>
2019-02-25 10:42:28 -0500

    refactored BlackJack so that it is easier to read

--- src/main/java/io/zipcoder/casino/Games/BlackJack.java
+++ src/main/java/io/zipcoder/casino/Games/BlackJack.java
@@ -50,76 +50,42 @@ public class BlackJack extends CardGames implements GamblingGame {
 
             dealCardsToPlayerAndDealerAndAddThemToRespectiveHands();
 
+            printUserFirstHandAndDealerFirstCard();
 
-            console.print("Your current cards: ");
-            console.println(playerHand.toString());
-            console.print("Your current hand value: ");
-            console.println(playerHand.getSumOfHand().toString());
-            console.print("The Dealers first card: ");
-            console.println(dealerHand.getACard(0).toString());
                 String userChoice = "";
             do {
                 userChoice = console.getStringInput("Do you want to (H)it or (S)tay: ");
                 if ("H".equals(userChoice.toUpperCase())){
-                    Card nextPlayerCard = cardDeck.dealCard();
-                    playerHand.addACard(nextPlayerCard);
-                    console.print("Your current cards");
-                    console.println(playerHand.toString());
-                    console.print("Your current hand value: ");
-                    console.println(playerHand.getSumOfHand().toString());
+                    dealACardToThePlayerAndPrintTheirNewHand();
                 }
             }while ("H".equals(userChoice.toUpperCase()) && playerHand.getSumOfHand() < 22 && !playerHand.getSumOfHand().equals(21));
 
             if (playerHand.getSumOfHand() > 21){
-                console.println("You lost this hand!");
-                console.println("You lost: " + pot);
-                console.println("Your new balance is: " + player.getBalance().getBalance());
+                playerLostHandWithValueLargerThan21();
                 dealer.collect(balance, pot);
             }
             else if (playerHand.getSumOfHand() == 21){
-                console.println("BLACKJACK!");
-                console.println("You win your bet back plus double of the pot: " + pot * 3);
-                player.setBalance(new Balance(new Integer(player.getBalance().getBalance().intValue() + (pot * 3))));
-                console.println("Your new balance is: " + player.getBalance().getBalance());
+                playerGotBlackJack();
             }
             else if ("S".equals(userChoice.toUpperCase())){
-                console.print("The dealers shows all cards: ");
-                console.println(dealerHand.toString());
-                console.print("The dealers hand value is: ");
-                console.println(dealerHand.getSumOfHand().toString());
+                printDealersFullHand();
+
                 while (dealerHand.getSumOfHand() < playerHand.getSumOfHand() && dealerHand.getSumOfHand() <= 21){
-                    Card dealerNextCard = cardDeck.dealCard();
-                    dealerHand.addACard(dealerNextCard);
-                    console.println("The dealers hits");
-                    console.print("The dealers hand is now: ");
-                    console.println(dealerHand.toString());
-                    console.print("The dealers hand value is now: ");
-                    console.println(dealerHand.getSumOfHand().toString());
+                    dealerDrawsACardAddsItToHisHandAndPrintDealerHand();
                 }
                 if (dealerHand.getSumOfHand() == 21){
-                    console.println("You lost this hand! The dealer has BlackJack!");
-                    console.println("You lost: " + pot);
-                    console.println("Your new balance is: " + player.getBalance().getBalance());
+                    dealerGotBlackJack();
                     dealer.collect(balance, pot);
                 }
                 else if (dealerHand.getSumOfHand() > 21){
-                    console.println("You Win! The Dealer Bust!");
-                    console.println("You collect double of the pot: " + pot * 2);
-                    player.setBalance(new Balance(new Integer(player.getBalance().getBalance().intValue() + (pot * 2))));
-                    console.println("Your new balance is: " + player.getBalance().getBalance());
+                    dealerBust();
                 }
                 else if (dealerHand.getSumOfHand() > playerHand.getSumOfHand()){
-                    console.println("The Dealer Stays");
-                    console.println("You lost this hand! The Dealers hand is larger than yours!");
-                    console.println("You lost: " + pot);
-                    console.println("Your new balance is: " + player.getBalance().getBalance());
+                    dealerHandBeatsPlayerHand();
                     dealer.collect(balance, pot);
                 }
                 else if (dealerHand.getSumOfHand() == playerHand.getSumOfHand()){
-                    console.println("Its a draw!");
-                    console.println("You get your bet back");
-                    player.setBalance(new Balance(new Integer(player.getBalance().getBalance().intValue() + pot)));
-                    console.println("Your new balance is: " + player.getBalance().getBalance());
+                    dealerHandAndDealerHandIsADraw();
                 }
             }
             String userInput = console.getStringInput("Would you like to continue? (Y/N)");
@@ -143,6 +109,81 @@ public class BlackJack extends CardGames implements GamblingGame {
         Card dealerCard = cardDeck.dealCard();
         dealerHand.addACard(dealerCard);
         playerHand.addACard(playerCard);
+    }
+
+    public void printUserFirstHandAndDealerFirstCard(){
+        console.print("Your current cards: ");
+        console.println(playerHand.toString());
+        console.print("Your current hand value: ");
+        console.println(playerHand.getSumOfHand().toString());
+        console.print("The Dealers first card: ");
+        console.println(dealerHand.getACard(0).toString());
+    }
+
+    public void dealACardToThePlayerAndPrintTheirNewHand(){
+        Card nextPlayerCard = cardDeck.dealCard();
+        playerHand.addACard(nextPlayerCard);
+        console.print("Your current cards");
+        console.println(playerHand.toString());
+        console.print("Your current hand value: ");
+        console.println(playerHand.getSumOfHand().toString());
+    }
+
+    public void playerLostHandWithValueLargerThan21(){
+        console.println("You lost this hand!");
+        console.println("You lost: " + pot);
+        console.println("Your new balance is: " + player.getBalance().getBalance());
+    }
+
+    public void playerGotBlackJack(){
+        console.println("BLACKJACK!");
+        console.println("You win your bet back plus double of the pot: " + pot * 3);
+        player.setBalance(new Balance(new Integer(player.getBalance().getBalance().intValue() + (pot * 3))));
+        console.println("Your new balance is: " + player.getBalance().getBalance());
+    }
+
+    public void printDealersFullHand(){
+        console.print("The dealers shows all cards: ");
+        console.println(dealerHand.toString());
+        console.print("The dealers hand value is: ");
+        console.println(dealerHand.getSumOfHand().toString());
+    }
+
+    public void dealerDrawsACardAddsItToHisHandAndPrintDealerHand(){
+        Card dealerNextCard = cardDeck.dealCard();
+        dealerHand.addACard(dealerNextCard);
+        console.println("The dealers hits");
+        console.print("The dealers hand is now: ");
+        console.println(dealerHand.toString());
+        console.print("The dealers hand value is now: ");
+        console.println(dealerHand.getSumOfHand().toString());
+    }
+
+    public void dealerGotBlackJack(){
+        console.println("You lost this hand! The dealer has BlackJack!");
+        console.println("You lost: " + pot);
+        console.println("Your new balance is: " + player.getBalance().getBalance());
+    }
+
+    public void dealerBust(){
+        console.println("You Win! The Dealer Bust!");
+        console.println("You collect double of the pot: " + pot * 2);
+        player.setBalance(new Balance(new Integer(player.getBalance().getBalance().intValue() + (pot * 2))));
+        console.println("Your new balance is: " + player.getBalance().getBalance());
+    }
+
+    public void dealerHandBeatsPlayerHand(){
+        console.println("The Dealer Stays");
+        console.println("You lost this hand! The Dealers hand is larger than yours!");
+        console.println("You lost: " + pot);
+        console.println("Your new balance is: " + player.getBalance().getBalance());
+    }
+
+    public void dealerHandAndDealerHandIsADraw(){
+        console.println("Its a draw!");
+        console.println("You get your bet back");
+        player.setBalance(new Balance(new Integer(player.getBalance().getBalance().intValue() + pot)));
+        console.println("Your new balance is: " + player.getBalance().getBalance());
     }
 
     public void collect() {
