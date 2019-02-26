475cc35ae43d77a412718106277b6f791fffbf3b
zipcodeFrancisco <zipcodefrancisco@gmail.com>
2019-02-26 16:21:43 -0500

    Decoration in process...

--- src/main/java/io/zipcoder/casino/DecorationCards.java
+++ src/main/java/io/zipcoder/casino/DecorationCards.java
@@ -1,15 +1,13 @@
 package io.zipcoder.casino;
 
-import io.zipcoder.casino.Games.DiceGames;
 import io.zipcoder.casino.utilities.Console;
 import io.zipcoder.casino.utilities.Symbols;
 
 
 public class DecorationCards {
 
-    Integer setDelay = 0;
     Console console ;
-
+    private static String containerT = "";
     private static String container1 = "";
     private static String container2 = "";
     private static String container3 = "";
@@ -22,22 +20,19 @@ public class DecorationCards {
 
     public void drawHand( Hand hand ){
         Symbols sym = new Symbols();
-        String suitOfCard , rankOfCard , containerT = "";
+        String suitOfCard , rankOfCard = "";
 
         for (int i = 0; i < hand.getNumberOfCards(); i++) {
             rankOfCard =  String.valueOf( hand.getACard(i).getRank() );
             suitOfCard =  String.valueOf( hand.getACard(i).getSuit() );
-
             container1 += sym.switchFrame("lU") + String.format( sym.gethL(5) ) + sym.switchFrame("rU") ;
             container2 += sym.switchFrame("vL") + String.format("%s", sym.switchRank(rankOfCard).length() == 2 ? sym.switchRank(rankOfCard) : sym.switchRank(rankOfCard) + " ") + "   "+ sym.switchFrame("vL");
             container3 += sym.switchFrame("vL") + String.format("  " + "%s"  + "  ", sym.switchSuit(suitOfCard) ) + sym.switchFrame("vL");
             container4 += sym.switchFrame("vL") + String.format("   %2.2s", sym.switchRank(rankOfCard).length() == 2 ? sym.switchRank(rankOfCard)  + "" : sym.switchRank(rankOfCard)) + sym.switchFrame("vL");
             container5 += sym.switchFrame("lD") + String.format( sym.gethL(5) + sym.switchFrame("rD") );
-
-            containerT = container1 + "\n" + container2 + "\n"
-                    + container3 + "\n" + container4 + "\n" + container5;
+            fillContainer();
         }
-        container1 = container2 = container3 = container4 = container5 = "";
+        clearString();
         console.print(containerT);
     }
 
@@ -50,11 +45,22 @@ public class DecorationCards {
             container3 += sym.switchDie(i + 1)[2];
             container4 += sym.switchDie(i + 1)[3];
             container5 += sym.switchDie(i + 1)[4];
-            containerT = container1 + "\n" + container2 + "\n"
-                    + container3 + "\n" + container4 + "\n" + container5;
+            fillContainer();
         }
-        container1 = container2 = container3 = container4 = container5 = "";
+        clearString();
         console.print(containerT);
     }
 
+    public void clearString(){
+        this.container1 = "";
+        this.container2 = "";
+        this.container3 = "";
+        this.container4 = "";
+        this.container5 = "";
+    }
+
+    public void fillContainer(){
+        this.containerT = container1 + "\n" + container2 + "\n"
+                + container3 + "\n" + container4 + "\n" + container5;
+    }
 }
